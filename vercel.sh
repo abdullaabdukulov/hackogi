d5c1d0e93fbd30ffc919e23c16a8e59b74e0924f
Abdulla <abduqulovabdulla3108@gmail.com>
2025-05-07 09:32:05 +0500

    fixed

--- vercel.sh
+++ vercel.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo "Installing dependencies..."
-pip3 install -r requirements.txt
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
-
 echo "Running makemigrations..."
 python3 manage.py makemigrations
 
