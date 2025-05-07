1f5a096cd75c86b8fb9a63e1a7e27e10729d8495
Abdulla <abduqulovabdulla3108@gmail.com>
2025-05-07 10:12:37 +0500

    done settings

--- hackogi/settings.py
+++ hackogi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_6h(2&47$x*k&b^vb($p@y^57pkz1(60ab+l%g+h@kj#!g1mjk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'programiz.uz']
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
